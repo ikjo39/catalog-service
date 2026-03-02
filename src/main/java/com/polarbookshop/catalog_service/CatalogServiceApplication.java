3758b27bac1e91ad40b4b3cbde4d78bb600facae
hyeong2g <hyeong2g@woowahan.com>
2026-03-02 22:33:26 +0900

    외부화 설정 관리 개념 추가

--- src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.polarbookshop.catalog_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan // 스프링 컨텍스트에 설정 데이터 빈을 로드
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
