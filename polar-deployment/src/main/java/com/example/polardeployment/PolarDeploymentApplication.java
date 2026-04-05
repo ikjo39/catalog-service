f157887ec4230c5ffa44b5a3e08c39067c6d201c
hyeong2g <hyeong2g@woowahan.com>
2026-04-05 19:18:39 +0900

    docker-compose로 편하게 띄우기 위한 구성 모듈 추가

---
+++ polar-deployment/src/main/java/com/example/polardeployment/PolarDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.polardeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarDeploymentApplication.class, args);
+    }
+
+}
---
+++ polar-deployment/src/test/java/com/example/polardeployment/PolarDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polardeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
